364d6d37d6f8076be8d13f9b7acc76ca0dc3cd1f
royeun <roy.eun01@gmail.com>
2016-06-28 14:58:02 -0700

    Complete deploying the server through express.js

---
+++ server/express-server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var handleRequest = require('./request-handler');
+
+var server = express();
+var port = 3000;
+
+server.get('/classes/messages', handleRequest.requestHandler);
+
+server.use(express.static('client/2016-06-chatterbox-client/client'));
+
+server.listen(port, function() {
+  console.log('listening to Duke');
+});
+
+// server.get('/', function(req, res) {
+//   console.log('got GET request');
+//   res.send('Thank you for asking, here is the information requested');
+// });
+
+// server.post('/', function(req, res) {
+//   res.send('Thank you for sending me something');
+// });
